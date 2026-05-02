30fe092e97fcb008ad1ee72b8bde225e11215643
Prithvi Dhiman <prithvidhiman231@gmail.com>
2026-05-02 15:25:22 +0530

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+/*LOAN CALCULATION */
 function calculateLoan() {
-    const loanAmountValue = parseFloat(document.getElementById("loan-amount").value);
-    const interestRateValue = parseFloat(document.getElementById("interest-rate").value);
-    const MonthsToPayValue = parseFloat(document.getElementById("months-to-pay").value);
+    const loanAmount = document.getElementById("loan-amount").value;
+    const interestRate = document.getElementById("interest-rate").value;
+    const monthsToPay = document.getElementById("months-to-pay").value;
 
-    const interest = (loanAmountValue * (interestRateValue * 0.01)) / MonthsToPayValue;
+    const monthlyInterest = interestRate / 100 / 12;
 
-    const monthlyPayment = (loanAmountValue / MonthsToPayValue + interest).toFixed(2);
+    const monthlyPayment =
+        (loanAmount * monthlyInterest) /
+        (1 - Math.pow(1 + monthlyInterest, -monthsToPay));
 
-    document.getElementById("payment").innerHTML = `Monthly Payment: ${monthlyPayment}`;
+    document.getElementById("payment").innerText =
+        "Monthly Payment ($): " + monthlyPayment.toFixed(2);
 }
+
+/* ABOUT SECTION TOGGLE*/
+const aboutBtn = document.getElementById("about-btn");
+const aboutSection = document.getElementById("about-section");
+
+aboutBtn.addEventListener("click", () => {
+    if (aboutSection.style.display === "block") {
+        aboutSection.style.display = "none";
+    } else {
+        aboutSection.style.display = "block";
+    }
+});
